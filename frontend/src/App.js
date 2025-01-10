185d092b6377528990e14d3a575e667c77345dfe
Lathees <latheesraja655@gmail.com>
2025-01-10 09:06:11 +0530

    feat: roadmap style homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import AdminSignup from './components/AdminSignup';
-import AdminHome from './components/AdminHome';
-import UserHome from './components/UserHome';
-import ForgotPassword from './components/ForgotPassword';
-import ResetPassword from './components/ResetPassword';
-import Profile from './components/Profile';
-import { AppBar, Toolbar, Button, Container, Box } from '@mui/material';
-import ErrorBoundary from './components/ErrorBoundary';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import AdminSignup from "./components/AdminSignup";
+import AdminHome from "./components/AdminHome";
+import UserHome from "./components/UserHome";
+import ForgotPassword from "./components/ForgotPassword";
+import ResetPassword from "./components/ResetPassword";
+import Profile from "./components/Profile";
+import { AppBar, Toolbar, Button, Container, Box } from "@mui/material";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   // Get current path
   const path = window.location.pathname;
-  const showNavBar = !['/user-home', '/admin-home', '/profile'].includes(path);
+  const showNavBar = !["/user-home", "/admin-home", "/profile"].includes(path);
 
   return (
     <ErrorBoundary>
@@ -32,7 +32,7 @@ function App() {
               </Toolbar>
             </AppBar>
           )}
-          
+
           <Container>
             <Routes>
               <Route path="/login" element={<Login />} />
